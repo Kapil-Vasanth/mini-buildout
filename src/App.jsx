e1bc6740f4df92d8432db55b9250f5142bb2e3fe
kapilvasanth <kapilvasanthbalerao@gmail.com>
2024-06-03 17:49:45 +0530

    Testing Route

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<>Working</>} />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
