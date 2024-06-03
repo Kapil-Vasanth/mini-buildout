4801ceaec79bb4b39d5e4de4e9ced955ecf2a42e
kapilvasanth <kapilvasanthbalerao@gmail.com>
2024-06-03 20:17:24 +0530

    Testing Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/Login"
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login from './components/Login'
 
-
-
+// App component containing the router
 function App() {
   return (
-    <>
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-      
-    </>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<h1>Mini Buildouts</h1>} />
+          <Route path="/login" element={<h1>Testing</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
