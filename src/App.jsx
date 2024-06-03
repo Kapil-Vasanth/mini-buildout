e5fe5855dc58811958f48e8bd7d01c580b9039b8
kapilvasanth <kapilvasanthbalerao@gmail.com>
2024-06-03 23:40:18 +0530

    Testing with Outlet Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './components/Login'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Outlet } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Outlet />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      }
+    ],
+  },
+]);
 
 // App component containing the router
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<h1>Mini Buildouts</h1>} />
-          <Route path="/login" element={<h1>Testing</h1>} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
