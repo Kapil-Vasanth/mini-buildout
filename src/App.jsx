091f2919307c3d80c5c69e6b0ba4c640a10cfa11
kapilvasanth <kapilvasanthbalerao@gmail.com>
2024-06-04 01:35:58 +0530

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
 import Login from './components/Login'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Outlet } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Outlet />
-  },
-  {
-    path:"/login",
-    element : <Login />
-  }
-]);
+
+
 
 // App component containing the router
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <Login />
     </>
   );
 }
